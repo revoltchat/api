50f083b56b06df4ec77fdc356ba974d4b4810e88
Paul <paulmakles@gmail.com>
2021-07-24 17:01:05 +0100

    0.5.1-alpha.10 Route: Changed ban member list to return users as well. Disabled incremental compilation / fixed .gitignore.

--- types/__temp.ts
+++
@@ -1,8 +0,0 @@
-
-                interface FetchSettingsOptions {
-                    /**
-                     * Keys to fetch
-                     */
-                    keys: string[]
-                }
-            
